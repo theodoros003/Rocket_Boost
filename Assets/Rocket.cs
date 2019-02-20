2f8e785d3db70d01bb994d5577a589a51039c10e
Theo003 <theodorosth95@gmail.com>
2019-02-20 04:20:32 +0000

    S3, Lecture 42 (Adding keys to the rocket A,D,Space)

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("spaced preced");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+    }
+}
