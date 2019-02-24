21c052f09750bd076fe7bd33959a4c6598ae39d9
Theo003 <theodorosth95@gmail.com>
2019-02-24 21:59:43 +0000

    S3, Lecture 48 (Movement and bug adjustments)

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -18,15 +18,16 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetInput();
+        Boost();
+        Rotate();
     }
 
-    private void GetInput()
+    private void Boost()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying)
+            if (!audioSource.isPlaying) // so it doesn't layer
             {
                 audioSource.Play();
             }
@@ -35,6 +36,12 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
@@ -43,5 +50,9 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
+
+    
 }
