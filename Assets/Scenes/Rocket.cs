4debee8aa9887e8cbbf6616025ff4550b09b7ae3
Theo003 <theodorosth95@gmail.com>
2019-02-25 02:37:06 +0000

    S3, Lecture 49-51 (Add serializeFields, Tags, Design)

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rotateSpeed = 120f;
+    [SerializeField] float boostSpeed = 100f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -22,11 +25,29 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("ok");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("boom");
+                break;
+        }
+        
+    }
+
     private void Boost()
     {
+
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * boostSpeed);
             if (!audioSource.isPlaying) // so it doesn't layer
             {
                 audioSource.Play();
@@ -41,14 +62,16 @@ public class Rocket : MonoBehaviour
     private void Rotate()
     {
         rigidBody.freezeRotation = true; // take manual control of rotation
+        
+        float frameSpeed = rotateSpeed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * frameSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * frameSpeed);
         }
 
         rigidBody.freezeRotation = false; // resume physics control of rotation
